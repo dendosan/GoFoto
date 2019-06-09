e5bf04f021e813b647554aed911559c86ba70697
dendosan <dendo74@hotmail.com>
2019-06-08 23:10:02 -0400

    store values in cookie

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"github.com/satori/go.uuid"
 	"html/template"
 	"net/http"
+	"strings"
 )
 
 var tpl *template.Template
@@ -20,7 +21,9 @@ func main() {
 
 func index(w http.ResponseWriter, req *http.Request) {
 	c := getCookie(w, req)
-	tpl.ExecuteTemplate(w, "index.gohtml", c.Value)
+	c = appendValue(w, c)
+	xs := strings.Split(c.Value, "|")
+	tpl.ExecuteTemplate(w, "index.gohtml", xs)
 }
 
 // add fund to get Cookie
@@ -34,5 +37,26 @@ func getCookie(w http.ResponseWriter, req *http.Request) *http.Cookie {
 		}
 		http.SetCookie(w, c)
 	}
+	return c
+}
+
+func appendValue(w http.ResponseWriter, c *http.Cookie) *http.Cookie {
+	// values
+	p1 := "disneyland.jpg"
+	p2 := "atbeach.jpg"
+	p3 := "hollywood.jpg"
+	// append
+	s := c.Value
+	if !strings.Contains(s, p1) {
+		s += "|" + p1
+	}
+	if !strings.Contains(s, p2) {
+		s += "|" + p2
+	}
+	if !strings.Contains(s, p3) {
+		s += "|" + p3
+	}
+	c.Value = s
+	http.SetCookie(w, c)
 	return c
 }
